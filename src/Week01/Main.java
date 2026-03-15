2e8130df454637f4e1ac99e255448152c2814c6f
bsharabi <bsharabi95@gmail.com>
2026-03-15 10:50:52 +0200

    week 01

--- src/Week01/Main.java
+++ src/Week01/Main.java
@@ -1,14 +1,39 @@
 package Week01;
 
+import java.util.Random;
+
 public class Main {
+    static Random rd = new Random();
 
     static void main() {
 
-
+        Node<Integer>
+                chain = new Node<>(1);
+        buildRand(chain,10);
+        printChain(chain);
 
     }
-    public static boolean isBinaryChain(Node<Integer> chain){
 
+    public static void buildRand(Node<Integer> chain, int size) {
+        if (chain == null)
+            return;
+        for (int i = 0; i < size; i++) {
+            Node<Integer> newNode = new Node<>(rd.nextInt(0, 2));
+            chain.setNext(newNode);
+            chain = chain.getNext();
+        }
+    }
+
+    public static void printChain(Node<Integer> chain) {
+        while (chain != null) {
+
+            System.out.print(chain.getVal() + " -> ");
+            chain = chain.getNext();
+        }
+        System.out.println("null");
+    }
+
+    public static boolean isBinaryChain(Node<Integer> chain) {
 
 
         return true;
