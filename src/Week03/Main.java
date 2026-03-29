98897c8a943e11da58afc590b6a444663158a1ba
bsharabi <bsharabi95@gmail.com>
2026-03-29 10:31:44 +0300

    Week03

---
+++ src/Week03/Main.java
@@ -0,0 +1,10 @@
+package Week03;
+
+
+import java.util.Stack;
+
+public class Main {
+    static void main() {
+
+    }
+}
---
+++ src/Week03/StackWithNode.java
@@ -0,0 +1,39 @@
+package Week03;
+
+public class StackWithNode<E> {
+
+    static String ID;
+    public StackWithNode() {
+    }
+
+
+    public E push(E item) {
+
+
+        return item;
+    }
+
+
+    public synchronized E pop() {
+        E obj;
+
+
+        return null;
+    }
+
+
+    public E peek() {
+        return null;
+    }
+
+
+    public boolean empty() {
+        return false;
+    }
+
+
+    public int search(Object o) {
+        return -1;
+    }
+
+}
