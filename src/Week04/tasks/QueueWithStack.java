ac9b6858726cf3e35d51f5a0289e613c848f4cc3
bsharabi <bsharabi95@gmail.com>
2026-04-12 12:43:55 +0300

    week04

--- src/Week04/tasks/QueueWithStack.java
+++ src/Week04/tasks/QueueWithStack.java
@@ -1,4 +1,112 @@
 package Week04.tasks;
 
-public class QueueWithStack {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+
+
+public class QueueWithStack<E> implements Queue<E> {
+
+    static String ID;
+
+    public QueueWithStack() {
+    }
+
+    @Override
+    public int size() {
+        //implement
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        //implement
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        //implement
+        return false;
+    }
+
+    @Override
+    public boolean add(E e) {
+        //implement
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        //implement
+        return false;
+    }
+    @Override
+    public void clear() {
+        //implement
+    }
+    @Override
+    public E remove() {
+        //implement
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        //implement
+        return null;
+    }
+    @Override
+    public E peek() {
+        //implement
+        return null;
+    }
+    //---------------------------------------------------------
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+
+    @Override
+    public boolean offer(E e) {
+        return false;
+    }
+
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
 }
--- src/Week04/tasks/StackWithQueue.java
+++ src/Week04/tasks/StackWithQueue.java
@@ -1,4 +1,40 @@
 package Week04.tasks;
 
-public class StackWithQueue {
+
+public class StackWithQueue<E> {
+
+    static String ID;
+    public StackWithQueue() {
+    }
+
+
+    public E push(E item) {
+
+
+        return item;
+    }
+
+
+    public synchronized E pop() {
+        E obj;
+
+
+        return null;
+    }
+
+
+    public E peek() {
+        return null;
+    }
+
+
+    public boolean empty() {
+        return false;
+    }
+
+
+    public int search(Object o) {
+        return -1;
+    }
+
 }
