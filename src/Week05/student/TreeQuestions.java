cbc6ca428c54f0edcc4a893bfdbdc4b14e919124
bsharabi <bsharabi95@gmail.com>
2026-04-19 11:16:35 +0300

    week05 task

---
+++ src/Week05/student/TreeQuestions.java
@@ -0,0 +1,61 @@
+package Week05.student;
+
+import Structures.BinNode;
+
+public class TreeQuestions {
+    public static String studentId = "";
+
+    // Question 12
+    public static int countGrandparents(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 13
+    public static int sumOddOnlyChildren(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 14
+    public static int countNodesWithTwoChildrenAndEqualValues(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 15
+    public static int countNodesSmallerThanAtLeastOneChild(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 16
+    public static int countNodesGreaterThanParent(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 17
+    public static int countNodesAtLevel(BinNode<Integer> t, int level) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 18
+    public static int sumNodesUntilLevelExclusive(BinNode<Integer> t, int level) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 19
+    public static int countOnlyChildrenFromLevel(BinNode<Integer> t, int level) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 20
+    public static int countNodesBetweenLevels(BinNode<Integer> t, int level1, int level2) {
+        // TODO: Implement this method.
+        return 0;
+    }
+}
---
+++ src/Week05/task/TreeQuestions.java
@@ -0,0 +1,43 @@
+package Week05.task;
+
+import Structures.BinNode;
+
+public class TreeQuestions{
+    public static String studentId = "";
+
+    // Question 6
+    public static int sumNodes(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 7
+    public static int countLeaves(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 8
+    public static int sumRightChildren(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 9
+    public static int countOnlyChildren(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 10
+    public static int countEvenValuedNodes(BinNode<Integer> t) {
+        // TODO: Implement this method.
+        return 0;
+    }
+
+    // Question 11
+    public static int countNodesEqualToX(BinNode<Integer> t, int x) {
+        // TODO: Implement this method.
+        return 0;
+    }
+}
