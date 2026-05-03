4df9a3377d0db646661460778d8b2ab0a9508c4b
bsharabi <bsharabi95@gmail.com>
2026-05-03 14:23:35 +0300

    update

---
+++ src/Week07/Mahat_Summer_2024B.java
@@ -0,0 +1,49 @@
+package Week07;
+
+import java.util.Scanner;
+
+public class Mahat_Summer_2024B {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        while (true) {
+            int num = sc.nextInt();
+            switch (num) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
+
